e204a12778c67047d79133e355810e14ceb391d3
Abubakar Yimam <abubakaryimam37@gmail.com>
2026-08-05 07:56:22 +0300

    init commit

---
+++ ContactsDataAccessLayer/ContactData.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace ContactsDataAccessLayer
+{
+    public class clsContactDataAccess
+    {
+        public static bool GetContactInfoByID(int ID, ref string FirstName, ref string LastName,
+            ref string Email, ref string Phone, ref string Address,
+            ref DateTime DateOfBirth, ref int CountryID, ref string ImagePath)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Contacts WHERE ContactID = @ContactID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ContactID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    // The record was found
+                    isFound = true;
+
+                    FirstName = (string)reader["FirstName"];
+                    LastName = (string)reader["LastName"];
+                    Email = (string)reader["Email"];
+                    Phone = (string)reader["Phone"];
+                    Address = (string)reader["Address"];
+                    DateOfBirth = (DateTime)reader["DateOfBirth"];
+                    CountryID = (int)reader["CountryID"];
+
+                    //ImagePath: allows null in database so we should handle null
+                    if (reader["ImagePath"] != DBNull.Value)
+                    {
+                        ImagePath = (string)reader["ImagePath"];
+                    }
+                    else
+                    {
+                        ImagePath = "";
+                    }
+
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+        public static int AddNewContact(string FirstName, string LastName, string Email, string Phone
+            , string Address, DateTime DateOfBirth, int CountryID, string ImagePath)
+        {
+            int result = -1;
+            SqlConnection sqlConnection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "INSERT INTO Contacts (FirstName, LastName, Email, Phone, Address,DateOfBirth, CountryID,ImagePath)\r\n                             VALUES (@FirstName, @LastName, @Email, @Phone, @Address,@DateOfBirth, @CountryID,@ImagePath);\r\n                             SELECT SCOPE_IDENTITY();";
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Phone", Phone);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+
+            if (ImagePath != null)
+            {
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@ImagePath", DBNull.Value);
+
+            }
+
+            try
+            {
+                sqlConnection.Open();
+                object obj = command.ExecuteScalar();
+
+                if (obj != null && int.TryParse(obj.ToString(), out var resualt2))
+                {
+                    result = resualt2;
+                }
+
+            }
+            catch (Exception) { }
+            finally { sqlConnection.Close(); }
+
+            return result;
+
+        }
+
+
+        public static bool UpdateContact(int ID, string FirstName, string LastName, string Email, string Phone, string Address, DateTime DateOfBirth, int CountryID, string ImagePath)
+        {
+            int num = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "Update  Contacts  \\r\\n                            set FirstName = @FirstName, \\r\\n                                LastName = @LastName, \\r\\n                                Email = @Email, \\r\\n                                Phone = @Phone, \\r\\n                                Address = @Address, \\r\\n                                DateOfBirth = @DateOfBirth,\\r\\n                                CountryID = @CountryID,\\r\\n                                ImagePath =@ImagePath\\r\\n                                where ContactID = @ContactID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ContactID", ID);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Phone", Phone);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+            if (ImagePath != "")
+            {
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@ImagePath", DBNull.Value);
+            }
+
+            try
+            {
+                connection.Open();
+                num = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return num > 0;
+
+
+        }
+
+        public static DataTable GetAllContacts()
+        {
+            DataTable dataTable = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * FROM Contacts";
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dataTable.Load(reader);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dataTable;
+
+        }
+
+
+        public static bool DeleteContact(int ContactID)
+        {
+            int num = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "Delete Contacts \r\n     where ContactID = @ContactID";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ContactID", ContactID);
+            try
+            {
+                connection.Open();
+                num = command.ExecuteNonQuery();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return num > 0;
+
+
+        }
+
+        public static bool ISContactExist(int ID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT Found=1 FRom Contacts Where ContactID=@CContactID";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@ContactID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader raeder = cmd.ExecuteReader();
+                isFound = raeder.HasRows;
+                raeder.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return isFound;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
